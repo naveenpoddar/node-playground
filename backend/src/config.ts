e2cb9f86731abfc20d5514e31b765e901a54b6e8
Naveen <poddarnaveen321@gmail.com>
2022-03-20 10:10:18 +0530

    first commit

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -5,4 +5,4 @@ export const CORS_ORIGINS = ["http://localhost:3000"];
 
 export const EXPOSED_PORT = "5858";
 export const CONTAINER_PORT = "7777";
-export const MONGO_URI = process.env.MONGO_URI;
+export const MONGO_URI = process.env.MONGO_URI as string;
