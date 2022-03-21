3c5d7ef6eaca7f08a0c606daa7a33feb7a0ce32a
Naveen <poddarnaveen321@gmail.com>
2022-03-21 17:02:07 +0530

    remove build folders

--- backend/build/lib/getContainerIP.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-async function getContainerIP(container) {
-    const inspect = await container.inspect();
-    const networkSettings = inspect.NetworkSettings;
-    const ip = networkSettings.IPAddress;
-    return ip;
-}
-exports.default = getContainerIP;
--- backend/build/tests/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const utf8_1 = __importDefault(require("utf8"));
-const str = "\x01\x00\x00\x00\x00\x00\x00\x1E";
-console.log(utf8_1.default.encode(str));
